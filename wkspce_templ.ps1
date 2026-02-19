e73a886ad2babf63335788c09e9ba9ba189b3c9f
support@testbenchlabs.com <110294487+ApplicationsEngineering@users.noreply.github.com>
2026-02-19 11:48:42 -0500

    Add files via upload

---
+++ wkspce_templ.ps1
@@ -0,0 +1,51 @@
+###############################################################################################################################################################
+# This is version 1.0 of ............................                                                                                                         #  
+# Courtesy of Testbench Labs LLC Web: https://www.testbenchlabs.com                                                                                           #  
+# email:support@TestbenchLabs                                                                                                                                 #                                      
+# github:https://github.com/ApplicationsEngineering/-Powershell"                                                                                              #
+#                                                                                                                                                             #
+# PREREQUISITES______________________________________________________________________________________________________________________________________________ #
+# Run the script from a Powershell Terminal with command: .\xxxxxxxx_xxx.ps1                                                                                  #                                  
+# This script will produce a report of hopefully something useful                                                                                             #
+# The report will include the following properties.............................................                                                               # 
+# Make sure you get a life so you wont me stuck doing this (kidding)                                                                                          #
+###############################################################################################################################################################
+
+
+$intellisense = "Isnt it Beautiful in VS Code, that there is no longer guess work no matter how much you train, make coding easier as my memory gets worse  : ) "
+Write-Host $intellisense
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#SANDBOX BELOW
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+
+
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+#< Prosposed PS Code and Snippets should be placed here for testing and development purposes. >
+
+
+
+
+###############################################################################################################################################################
